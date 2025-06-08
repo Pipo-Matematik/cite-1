4b31b4c49f3cdef4d16c6b9cdba0619a986b67e8
Pipo-Matematik <tsalihov019@gmail.com>
2025-06-08 16:05:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const GoToSite = document.getElementById("started-list")
+const GoToSite1 = document.getElementById("body-site")
+
+function showSite() {
+    GoToSite.style.display = "none"
+    GoToSite1.style.display = "block"
+}
