71045ef2d4434eb8ee225ff53619ac1dfb88aac3
Pipo-Matematik <tsalihov019@gmail.com>
2025-06-08 17:29:26 +0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const GoToSite = document.getElementById("started-list")
-const GoToSite1 = document.getElementById("body-site")
-
-function showSite() {
-    GoToSite.style.display = "none"
-    GoToSite1.style.display = "block"
-}
