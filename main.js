efab08d0b3690659ab9a34a49054b3adebda48a0
Pipo-Matematik <tsalihov019@gmail.com>
2025-06-10 17:09:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,162 @@
+const GoToSite = document.getElementById("started-list")
+const GoToSite1 = document.getElementById("body-site")
+const r00 = document.getElementById("r00")
+const r01 = document.getElementById("r01")
+const r02 = document.getElementById("r02")
+const r03 = document.getElementById("r03")
+const r04 = document.getElementById("r04")
+
+function showSite() {
+    GoToSite.style.display = "none"
+    GoToSite1.style.display = "block"
+}
+function showR01() {
+    r01.style.display = "block"
+    r02.style.display = "none"
+    r03.style.display = "none"
+    r04.style.display = "none"
+    r00.style.display = "none"
+}
+function showR02() {
+    r01.style.display = "none"
+    r02.style.display = "block"
+    r03.style.display = "none"
+    r04.style.display = "none"
+    r00.style.display = "none"
+}
+function showR03() {
+    r01.style.display = "none"
+    r02.style.display = "none"
+    r03.style.display = "block"
+    r04.style.display = "none"
+    r00.style.display = "none"
+}
+function showR04() {
+    r01.style.display = "none"
+    r02.style.display = "none"
+    r03.style.display = "none"
+    r04.style.display = "block"
+    r00.style.display = "none"
+}
+
+
+const hb1 = document.getElementById("hb-1")
+const mi1 = document.getElementById("mi-1")
+
+function ShowHB1() {
+    hb1.style.display = "block"
+    mi1.style.height = "310px"
+}
+function HideHB1() {
+    hb1.style.display = "none"
+    mi1.style.height = "110px"
+} 
+
+const hb2 = document.getElementById("hb-2")
+const mi2 = document.getElementById("mi-2")
+
+function ShowHB2() {
+    hb2.style.display = "block"
+    mi2.style.height = "285px"
+}
+function HideHB2() {
+    hb2.style.display = "none"
+    mi2.style.height = "110px"
+}
+
+const hb3 = document.getElementById("hb-3")
+const mi3 = document.getElementById("mi-3")
+
+function ShowHB3() {
+    hb3.style.display = "block"
+    mi3.style.height = "310px"
+}
+function HideHB3() {
+    hb3.style.display = "none"
+    mi3.style.height = "110px"
+}
+
+const hb4 = document.getElementById("hb-4")
+const mi4 = document.getElementById("mi-4")
+
+function ShowHB4() {
+    hb4.style.display = "block"
+    mi4.style.height = "285px"
+}
+function HideHB4() {
+    hb4.style.display = "none"
+    mi4.style.height = "110px"
+}
+
+const hb5 = document.getElementById("hb-5")
+const mi5 = document.getElementById("mi-5")
+
+function ShowHB5() {
+    hb5.style.display = "block"
+    mi5.style.height = "310px"
+}
+function HideHB5() {
+    hb5.style.display = "none"
+    mi5.style.height = "110px"
+}
+
+const hb6 = document.getElementById("hb-6")
+const mi6 = document.getElementById("mi-6")
+
+function ShowHB6() {
+    hb6.style.display = "block"
+    mi6.style.height = "285px"
+}
+function HideHB6() {
+    hb6.style.display = "none"
+    mi6.style.height = "110px"
+}
+
+const hb7 = document.getElementById("hb-7")
+const mi7 = document.getElementById("mi-7")
+
+function ShowHB7() {
+    hb7.style.display = "block"
+    mi7.style.height = "285px"
+}
+function HideHB7() {
+    hb7.style.display = "none"
+    mi7.style.height = "110px"
+}
+
+const hb8 = document.getElementById("hb-8")
+const mi8 = document.getElementById("mi-8")
+
+function ShowHB8() {
+    hb8.style.display = "block"
+    mi8.style.height = "310px"
+}
+function HideHB8() {
+    hb8.style.display = "none"
+    mi8.style.height = "110px"
+}
+
+const hb9 = document.getElementById("hb-9")
+const mi9 = document.getElementById("mi-9")
+
+function ShowHB9() {
+    hb9.style.display = "block"
+    mi9.style.height = "310px"
+}
+function HideHB9() {
+    hb9.style.display = "none"
+    mi9.style.height = "110px"
+}
+
+const hb10 = document.getElementById("hb-10")
+const mi10 = document.getElementById("mi-10")
+
+function ShowHB10() {
+    hb10.style.display = "block"
+    mi10.style.height = "310px"
+}
+function HideHB10() {
+    hb10.style.display = "none"
+    mi10.style.height = "110px"
+}
+
